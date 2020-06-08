9b2b65e6f65b1d7a505ae559d12f2c32fc8f1155
M4cs <mabridgland@protonmail.com>
2020-06-08 14:57:31 -0400

    Rename

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-env GOOS=darwin GOARCH=amd64 go build -o walgur-darwin=amd64
+env GOOS=darwin GOARCH=amd64 go build -o walgur-darwin-amd64
 env GOOS=darwin GOARCH=386 go build -o walgur-darwin-i386
 env GOOS=linux GOARCH=amd64 go build -o walgur-linux-amd64
 env GOOS=linux GOARCH=386 go build -o walgur-linux-i386
