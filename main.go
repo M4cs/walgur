5e7329a04a589b66f27992dfd9fe6ade4bbabd62
M4cs <mabridgland@protonmail.com>
2020-06-08 12:37:13 -0400

    Add Error Handling and Clean Up

--- main.go
+++ main.go
@@ -17,8 +17,6 @@ var (
 	url      string
 )
 
-// Authorization: Client-ID ec55a6eb90d209b
-
 type Result struct {
 	Data struct {
 		Images []struct {
@@ -30,16 +28,36 @@ type Result struct {
 func main() {
 	var args []string = os.Args
 	if len(args) <= 1 {
-		panic("Please Add An Imgur Gallery Hash: wallgur.exe {hash}")
+		fmt.Println("Please Add An Imgur Gallery Hash: wallgur.exe {hash}")
+		os.Exit(1)
 	}
 	var hash string = args[1]
 	client := &http.Client{}
-	req, _ := http.NewRequest("GET", "https://api.imgur.com/3/gallery/album/"+hash, nil)
+	req, err := http.NewRequest("GET", "https://api.imgur.com/3/gallery/album/"+hash, nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	req.Header.Set("Authorization", "Client-ID ec55a6eb90d209b")
-	res, _ := client.Do(req)
-	body, _ := ioutil.ReadAll(res.Body)
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if res.StatusCode == 404 {
+		fmt.Println("Error: Gallery Not Found")
+		os.Exit(1)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	var r Result
-	json.Unmarshal([]byte(string(body)), &r)
+	err = json.Unmarshal([]byte(string(body)), &r)
+	if err != nil {
+		fmt.Println("Unable to decode JSON from Imgur. Please try again!")
+	}
 	rand.Seed(time.Now().Unix())
 	name := fmt.Sprint(r.Data.Images[rand.Intn(len(r.Data.Images))].Tag, ".jpg")
 	url := fmt.Sprint("https://imgur.com/", name)
