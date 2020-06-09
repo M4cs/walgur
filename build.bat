a3d661877c92ec5a42a1762fed9e7244485fe34e
M4cs <mabridgland@protonmail.com>
2020-06-09 13:29:54 -0400

    Add Git Ignore

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-env GOOS=darwin GOARCH=amd64 go build -o walgur-darwin-amd64
-env GOOS=darwin GOARCH=386 go build -o walgur-darwin-i386
-env GOOS=linux GOARCH=amd64 go build -o walgur-linux-amd64
-env GOOS=linux GOARCH=386 go build -o walgur-linux-i386
-env GOOS=windows GOARCH=386 go build -o walgur-windows-i386.exe
-env GOOS=windows GOARCH=amd64 go build -o walgur-windows-amd64.exe
+env GOOS=darwin GOARCH=amd64 go build -o build/walgur-darwin-amd64
+env GOOS=darwin GOARCH=386 go build -o build/walgur-darwin-i386
+env GOOS=linux GOARCH=amd64 go build -o build/walgur-linux-amd64
+env GOOS=linux GOARCH=386 go build -o build/walgur-linux-i386
+env GOOS=windows GOARCH=386 go build -o build/walgur-windows-i386.exe
+env GOOS=windows GOARCH=amd64 go build -o build/walgur-windows-amd64.exe
