610e67df8cb9d0705e2395558f914b2ce44af3d2
Rusty Balboa <rustybalboadev@gmail.com>
2020-10-06 09:44:13 -0500

    Added Reddit Feature

--- main.go
+++ main.go
@@ -14,19 +14,27 @@ var (
 
 func main() {
 	parser := argparse.NewParser("walgur", "Set your wallpaper randomly from Imgur Galleries, Albums, and Subreddits.")
-	url := parser.String("u", "url", &argparse.Options{Required: false, Help: "Imgur URL to grab from.", Default: "Empty"})
+	reddit := parser.String("r", "reddit", &argparse.Options{Required: false, Help: "Reddit Link", Default: "None"})
+	imgur := parser.String("i", "imgur", &argparse.Options{Required: false, Help: "Imgur Link", Default: "None"})
 	var show *bool = parser.Flag("s", "show-bg", &argparse.Options{Required: false, Help: "Show where background is stored", Default: false})
 	var ver *bool = parser.Flag("v", "version", &argparse.Options{Required: false, Help: "Display walgur Version", Default: false})
 	err := parser.Parse(os.Args)
 	if err != nil {
 		fmt.Print(parser.Usage(err))
 	}
+
 	if *ver {
 		fmt.Println("walgur v1.3.0")
 	}
-	if *url != "Empty" {
-		query, typeOfQuery := getQuery(url)
-		body := makeQueryRequest(query)
+
+	if *reddit != "None" && *imgur == "None" {
+		//Reddit
+		body := makeQueryRequest(*reddit, "reddit")
+		changeWallpaper("reddit", body, show)
+	} else if *imgur != "None" && *reddit == "None" {
+
+		query, typeOfQuery := getQuery(*imgur)
+		body := makeQueryRequest(query, "imgur")
 		changeWallpaper(typeOfQuery, body, show)
 	}
 }
